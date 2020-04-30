6656225c2fabce114650dc3cff6d32bd657f8b0e
bpalle <bpalle@GVDIACN-2.na.gilead.com>
2020-04-30 10:27:15 +0530

    Added methods for windows server service verifications

--- AutoFiles/Run.bat
+++ AutoFiles/Run.bat
@@ -1,1 +1,2 @@
-PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'" > psoutput.txt
+PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
+

0f55715a9be62ee8c88982d372eca4f31ab1176b
nnereddula <nnereddula@GVDIACN-45.na.gilead.com>
2020-05-06 20:43:29 +0530

    update

--- src/test/java/com/qa/wave1/WinSer.java
+++ src/test/java/com/qa/wave1/WinSer.java
@@ -51,7 +51,7 @@ public class WinSer extends CommonMethods {
 		String resout = path + "Output.txt";
 		String runbat = path + "Run.bat";
 		
-		createNeededFiles("SJCONAPPPRDN02", "wuauserv");
+		createNeededFiles("CCCONAPPPRDN01", "wuauserv");
 		
 		//String password = RSA_Authentication("nnereddula");
 		String password = "pjBElwPk$7V*"; 

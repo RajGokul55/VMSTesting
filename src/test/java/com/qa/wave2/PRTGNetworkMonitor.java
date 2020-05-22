26e412b411ca8c9622fd9ad7d5442ef157985f79
dmarkanti <dmarkanti@GVDIACN-44.na.gilead.com>
2020-05-22 13:56:54 +0530

    PRTG Application Updates

--- src/test/java/com/qa/wave2/PRTGNetworkMonitor.java
+++ src/test/java/com/qa/wave2/PRTGNetworkMonitor.java
@@ -12,24 +12,32 @@ import org.testng.Assert;
 
 public class PRTGNetworkMonitor extends CommonMethods {
 
-
-	/***************************** Test Cases  *******************/
+	/***************************** Test Cases *******************/
 
 	@BeforeMethod(enabled = true)
 	public void BeforeMethod() throws IOException {
-		
+
 	}
-	
+
 	@Test(priority = 0, enabled = true)
-	public void PRTGNM_S01_Web_UserLogin() throws IOException {
+	public void PRTGNM_S01_Web_Devurl_UserLogin() throws IOException {
 		GM_PRTGNM_Web_DevURLLogin();
 	}
 
-	/*
-	 * @Test(priority = 1, enabled = true) public void
-	 * PRTGNM_S02_Web_ViewAllSensorsData() throws IOException {
-	 * TM_PRTGNM_S02_Web_ViewAllSensorsData(); }
-	 */
+	@Test(priority = 1, enabled = true)
+	public void PRTGNM_S02_Web_DevurlViewAllSensorsData() throws IOException {
+		TM_PRTGNM_S02_Web_Devurl_ViewAllSensorsData();
+	}
+
+	@Test(priority = 2, enabled = true)
+	public void PRTGNM_S03_Web_Produrl_UserLogin() throws IOException {
+		GM_PRTGNM_Web_ProdURLLogin();
+	}
+
+	@Test(priority = 3, enabled = true)
+	public void PRTGNM_S04_Web_Produrl_ViewAllSensorsData() throws IOException {
+		TM_PRTGNM_S04_Web_Produrl_ViewAllSensorsData();
+	}
 
 	@AfterMethod
 	public void afterEveryTest() {
@@ -41,87 +49,86 @@ public class PRTGNetworkMonitor extends CommonMethods {
 	By ipt_Username = By.xpath("//input[@id='loginusername']");
 	By ipt_Password = By.xpath("//input[@id='loginpassword']");
 	By btn_Login = By.xpath("//button[@class='loginbutton button big']");
-	
 
 	/***************************** Test Case Methods *******************/
 
-	/*
-	 * public void TM_PRTGNM_S01_Web_UserLogin() throws IOException { WebElement
-	 * Username_Ipt = createWebElementBy(ipt_Username); Username_Ipt.clear();
-	 * Username_Ipt.sendKeys(Username);
-	 * 
-	 * WebElement Password_Ipt = createWebElementBy(ipt_Password);
-	 * Password_Ipt.clear(); Password_Ipt.sendKeys(DecryptPassword);
-	 * 
-	 * WebElement Login_Btn = createWebElementBy(btn_Login); Login_Btn.click();
-	 * 
-	 * 
-	 * waitForPageLoaded(); takeScreenshotAtEndOfTest();
-	 * //Assert.assertEquals(driver.getTitle(), "");
-	 * 
-	 * }
-	 */
-	/*
-	 * public void TM_PRTGNM_S02_Web_ViewAllSensorsData() throws IOException {
-	 * 
-	 * WebElement Username_Ipt = createWebElementBy(ipt_Username);
-	 * Username_Ipt.sendKeys(Username);
-	 * 
-	 * WebElement Password_Ipt = createWebElementBy(ipt_Password);
-	 * Password_Ipt.sendKeys(DecryptPassword);
-	 * 
-	 * 
-	 * WebElement Assets_Menu = createWebElementBy(abc); //mouseHover(abc);
-	 * 
-	 * WebElement Computers_Menu = createWebElementBy(abcd); //mouseHover(abcd);
-	 * Computers_Menu.click();
-	 * 
-	 * 
-	 * 
-	 * 
-	 * waitForPageLoaded(); takeScreenshotAtEndOfTest();
-	 * Assert.assertEquals(driver.getTitle(), "");
-	 * 
-	 * }
-	 */
+	public void TM_PRTGNM_S02_Web_Devurl_ViewAllSensorsData() throws IOException {
+		GM_PRTGNM_Web_DevURLLogin();
+		waitForPageLoaded();
+		Assert.assertEquals(driver.getTitle(), "Welcome | PRTG Network Monitor (SJPRTGAPPDEVG01)",
+				"PRTG Network Monitor Home page not displayed");
+		takeScreenshotAtEndOfTest();
+	}
+
+	public void TM_PRTGNM_S04_Web_Produrl_ViewAllSensorsData() throws IOException {
+
+		GM_PRTGNM_Web_ProdURLLogin();
+		waitForPageLoaded();
+		Assert.assertEquals(driver.getTitle(), "Welcome | PRTG Network Monitor (SJPRTGAPPPRDN01)",
+				"PRTG Network Monitor Home page not displayed");
+		takeScreenshotAtEndOfTest();
+
+	}
 
 	/***************************** Generic Methods *******************/
-	
-	public void GM_PRTGNM_Web_DevURLLogin() throws IOException
-	{
-		
+
+	public void GM_PRTGNM_Web_DevURLLogin() throws IOException {
+
 		final String Browser = "Chrome";
 		final String DevURL = "http://sjprtgappdevg01/";
 		final String Username = "VMS";
 		String DecryptPassword = null;
 		final String EncryptPassword = "dLYEuUGwlY262xhKocrIUw==";
-		final String URL = DevURL; // Change URL whichever Environment you want to execute.
+		final String URL = DevURL; 
 		launchBrowser(Browser, URL);
-		try{
+		try {
 			DecryptPassword = decrypt(EncryptPassword);
-		} catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		WebElement Username_Ipt = createWebElementBy(ipt_Username);
 		Username_Ipt.clear();
 		Username_Ipt.sendKeys(Username);
-		
-		  WebElement Password_Ipt = createWebElementBy(ipt_Password);
-		  Password_Ipt.clear();
-		  Password_Ipt.sendKeys(DecryptPassword);
-		  
-		  WebElement Login_Btn = createWebElementBy(btn_Login); 
-		  Login_Btn.click();
-		 
+
+		WebElement Password_Ipt = createWebElementBy(ipt_Password);
+		Password_Ipt.clear();
+		Password_Ipt.sendKeys(DecryptPassword);
+
+		WebElement Login_Btn = createWebElementBy(btn_Login);
+		Login_Btn.click();
 
 		waitForPageLoaded();
 		takeScreenshotAtEndOfTest();
-		//Assert.assertEquals(driver.getTitle(), "");
 
-		}
 	}
-	
-	
-	
-	
 
+	public void GM_PRTGNM_Web_ProdURLLogin() throws IOException {
+
+		final String Browser = "Chrome";
+		final String ProdURL = "http://sjprtgappprdn01/";
+		final String Username = "dmarkanti";
+		String DecryptPassword = null;
+		final String EncryptPassword = "gauEQnlth0rzudo2rv35/A==";
+		final String URL = ProdURL;
+		launchBrowser(Browser, URL);
+		try {
+			DecryptPassword = decrypt(EncryptPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		WebElement Username_Ipt = createWebElementBy(ipt_Username);
+		Username_Ipt.clear();
+		Username_Ipt.sendKeys(Username);
+
+		WebElement Password_Ipt = createWebElementBy(ipt_Password);
+		Password_Ipt.clear();
+		Password_Ipt.sendKeys(DecryptPassword);
+
+		WebElement Login_Btn = createWebElementBy(btn_Login);
+		Login_Btn.click();
+
+		waitForPageLoaded();
+		takeScreenshotAtEndOfTest();
+
+	}
+}

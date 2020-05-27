1236eb669c19cf6083083859e619fa34cf75688f
bpalle <bpalle@GVDIACN-2.na.gilead.com>
2020-05-27 18:21:35 +0530

    Minor Updates

--- src/test/java/com/qa/wave2/DruvaInsyncECDB.java
+++ src/test/java/com/qa/wave2/DruvaInsyncECDB.java
@@ -6,20 +6,30 @@ import com.qa.demo.base.CommonMethods;
 public class DruvaInsyncECDB extends CommonMethods {
 
 
-	@Test(enabled = true, priority =1)
+	@Test(enabled = false, priority =1, groups = "Dev")
 	public void DruvaInsyncECDB_PS01_WinServ_SJGCDBWEBPOCN01() {
 		verifyWinSerStatus("SJGCDBWEBPOCN01", "Druva inSync AD Connector", "Running", "Auto");
 		verifyWinSerStatus("SJGCDBWEBPOCN01", "Druva inSync Client Service", "Running", "Auto");
 	}
 	
-	@Test(enabled = true, priority =2)
+	@Test(enabled = true, priority =2, groups = "Prod")
 	public void DruvaInsyncECDB_PS02_WinServ_SJECDBCONPRDN01() {
 		verifyWinSerStatus("sjECDBCONPRDN01", "Druva InSync AD Connector", "Running", "Auto");
 	}
 	
-	@Test(enabled = true, priority =3)
+	@Test(enabled = false, priority =3, groups = "Prod")
 	public void DruvaInsyncECDB_PS03_WinServ_DRECDBCONPRDN01() {
 		verifyWinSerStatus("DRECDBCONPRDN01", "Druva inSync AD Connector", "Running", "Auto");
+	}
+	
+	
+	@Test(enabled = true, priority =2, groups = "Test")
+	public void CLM_EOCONAPPPRDN02() {
+		verifyWinSerStatus("EOCONAPPPRDN02", "ArcSight Microsoft Windows Event Log - Unified", "Running", "Auto");
+		verifyWinSerStatus("EOCONAPPPRDN02", "ArcSight Microsoft Windows Event Log - Unified2", "Running", "Auto");
+		verifyWinSerStatus("EOCONAPPPRDN02", "ArcSight Microsoft DNS Trace Log Multiple Server File", "Running", "Auto");
+				
+
 	}
 	
 }

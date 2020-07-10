57781ba750cac3d294ad3e2e33da6d048db16484
abhatt2 <abhatt2@GVDIACN-63.na.gilead.com>
2020-07-10 17:47:35 +0530

    Adding a new method to connect to RDP machine

--- src/main/java/com/qa/demo/base/CommonMethods.java
+++ src/main/java/com/qa/demo/base/CommonMethods.java
@@ -830,7 +830,51 @@ public class CommonMethods {
 		} 
 
 	}
+	
+	public static void RDP_Connection(String servername,String username, String password) {
+		final String userDirectory = System.getProperty("user.dir");
+		String path = userDirectory + "\\AutoFiles\\SiKuli\\RDP\\";
+		Screen s = new Screen();
+		Pattern startBtn = new Pattern(path + "StartButton.PNG");
+		Pattern RDPConnection= new Pattern(path + "remoteConnection.PNG");
+		Pattern serverName= new Pattern(path + "RDPServerName.PNG");
+		Pattern connectRDPServer= new Pattern(path + "connectButton.PNG");
+		Pattern useanotherAccount= new Pattern(path + "useAnotherAccount.PNG");
+		Pattern userNameRDP= new Pattern(path + "usernameRDP.PNG");
+		Pattern passwordRDP= new Pattern(path + "passwordRDP.PNG");
+		Pattern okButton= new Pattern(path + "oKButtonRDP.PNG");
+		
+		
+		try {
+		s.wait(startBtn, 3000);
+		s.click();
+		s.wait(RDPConnection, 2000);
+		s.click();
+		s.wait(serverName, 2000);
+		s.type(servername);
+		s.wait(connectRDPServer, 2000);
+		s.click();
+		s.wait(useanotherAccount, 2000);
+		s.click();
+		s.wait(userNameRDP, 2000);
+		s.type(userNameRDP, username);
+		s.wait(passwordRDP, 2000);
+		s.type(passwordRDP, password);
+		s.wait(okButton, 2000);
+		s.click();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+		
+		
+	}
+	
+	
 }
+
+
 
 
 

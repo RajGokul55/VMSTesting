8abfa1eccf6bdefc90f8be35ca3e1473be255a16
bpalle <bpalle@GVDIACN-2.na.gilead.com>
2020-08-13 12:57:30 +0530

    Updated

--- AutoFiles/Run.ps1
+++ AutoFiles/Run.ps1
@@ -1,3 +1,8 @@
-$cred = get-Credential -credential administrator 
- $computer = get-content C:\Users\bpalle\git\SeleniumAutomationV0.1\AutoFiles\computers.txt
- Get-WMIObject Win32_Service -computer $computer -credential $cred | Where { $_.Name -eq "wuauserv" } | Out-File -FilePath C:\Users\bpalle\git\SeleniumAutomationV0.1\AutoFiles\Output.txt
+$Username = 'NA\ADM_nnereddula' 
+$Password = ConvertTo-SecureString 'Ftr%@4Wp%8El' -AsPlainText -Force 
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist ($Username, $Password)
+$computer = 'SECONAPPPRDN02' 
+$servename1 = 'arc_sena2_dns_tracelog' 
+$servename2 = 'arc_winc' 
+Get-WMIObject Win32_Service -computer $computer -credential $cred | Where { $_.Name -eq $servename1 -or  $_.Name -eq $servename2 } | Out-File -FilePath C:\Users\bpalle\git\SeleniumAutomationV0.1\AutoFiles\Output.txt
+PAUSE

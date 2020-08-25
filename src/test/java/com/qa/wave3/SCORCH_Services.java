e55f566ca5db47cffe0161809705aa1d891fa513
dmarkanti <dmarkanti@GVDIACN-44.na.gilead.com>
2020-08-25 20:30:40 +0530

    Scorch services update

--- src/test/java/com/qa/wave3/SCORCH_Services.java
+++ src/test/java/com/qa/wave3/SCORCH_Services.java
@@ -6,57 +6,58 @@ import com.qa.demo.base.CommonMethods;
 public class SCORCH_Services extends CommonMethods {
 
 
-	@Test(enabled = true, priority =1, groups = "Dev")
+	@Test(enabled = false, priority =1, groups = "Dev")
 	public void SCORCH_Dev_ManagementServer_sjscrhappdevn01() {
-		verifyWinSerStatus("sjscrhappdevn01.na.gilead.com", "Orchestrator Management Service", "Running","Auto");
-		verifyWinSerStatus("sjscrhappdevn01.na.gilead.com", "Orchestrator Remoting Service", "Running", "Auto");
-		verifyWinSerStatus("sjscrhappdevn01.na.gilead.com", "Orchestrator Runbook Server Monitor", "Running","Auto");
-		verifyWinSerStatus("sjscrhappdevn01.na.gilead.com", "Orchestrator Runbook Service", "Running","Auto");
+		verifyWinSerStatus("sjscrhappdevn01", "Orchestrator Management Service", "Running","Auto");
+		verifyWinSerStatus("sjscrhappdevn01", "Orchestrator Remoting Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappdevn01", "Orchestrator Runbook Server Monitor", "Running","Auto");
+		verifyWinSerStatus("sjscrhappdevn01", "Orchestrator Runbook Service", "Running","Auto");
 		
 	}
 	
 	@Test(enabled = false, priority =2, groups = "Dev")
 	public void SCORCH_Dev_RunbookServer_sjscrhappdevn02() {
-		verifyWinSerStatus("sjscrhappdevn02.na.gilead.com", "Orchestrator Management Service", "Running","Auto");
-		verifyWinSerStatus("sjscrhappdevn02.na.gilead.com", "Orchestrator Remoting Service", "Running", "Auto");
-		verifyWinSerStatus("sjscrhappdevn02.na.gilead.com", "Orchestrator Runbook Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappdevn02", "Orchestrator Management Service", "Running","Auto");
+		verifyWinSerStatus("sjscrhappdevn02", "Orchestrator Remoting Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappdevn02", "Orchestrator Runbook Service", "Running", "Auto");
 		
 	}
 	
-	@Test(enabled = false, priority =3, groups = "Prod")
+	@Test(enabled = true, priority =3, groups = "Prod")
 	public void SCORCH_Prod_ManagementServer_sjscrhappprdn01() {
-		verifyWinSerStatus("sjscrhappprdn01.na.gilead.com", "Orchestrator Management Service", "Running","Auto");
-		verifyWinSerStatus("sjscrhappprdn01.na.gilead.com", "Orchestrator Remoting Service", "Running", "Auto");
-		verifyWinSerStatus("sjscrhappprdn01.na.gilead.com", "Orchestrator Runbook Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappprdn01", "Orchestrator Management Service", "Running","Auto");
+		verifyWinSerStatus("sjscrhappprdn01", "Orchestrator Remoting Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappprdn01", "Orchestrator Runbook Service", "Running", "Auto");
+		
 		
 	}
 	
-	@Test(enabled = false, priority =4, groups = "Prod")
+	@Test(enabled = true, priority =4, groups = "Prod")
 	public void SCORCH_Prod_RunbookServer_sjscrhappprdn02() {
-		verifyWinSerStatus("sjscrhappprdn02.na.gilead.com", "Orchestrator Remoting Service", "Running", "Auto");
-		verifyWinSerStatus("sjscrhappprdn02.na.gilead.com", "Orchestrator Runbook Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappprdn02", "Orchestrator Remoting Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappprdn02", "Orchestrator Runbook Service", "Running", "Auto");
 		
 	}
 	
-	@Test(enabled = false, priority =5, groups = "Prod")
+	@Test(enabled = true, priority =5, groups = "Prod")
 	public void SCORCH_Prod_RunbookServer_sjscrhappprdn03() {
-		verifyWinSerStatus("sjscrhappprdn03.na.gilead.com", "Orchestrator Remoting Service", "Running", "Auto");
-		verifyWinSerStatus("sjscrhappprdn03.na.gilead.com", "Orchestrator Runbook Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappprdn03", "Orchestrator Remoting Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappprdn03", "Orchestrator Runbook Service", "Running", "Auto");
 		
 	}
 	
-	@Test(enabled = false, priority =6, groups = "Prod")
+	@Test(enabled = true, priority =6, groups = "Prod")
 	public void SCORCH_Prod_RunbookDesignerServer_sjscrhappprdn04() {
-		verifyWinSerStatus("sjscrhappprdn04.na.gilead.com", "Orchestrator Remoting Service", "Running", "Auto");
-		verifyWinSerStatus("sjscrhappprdn04.na.gilead.com", "Orchestrator Runbook Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappprdn04", "Orchestrator Remoting Service", "Running", "Auto");
+		verifyWinSerStatus("sjscrhappprdn04", "Orchestrator Runbook Service", "Running", "Auto");
 		
 	}
 	
 	@Test(enabled = false, priority =7, groups = "DR")
 	public void SCORCH_DR_RunbookManagementServer_drscrhappprdn01() {
-		verifyWinSerStatus("drscrhappprdn01.na.gilead.com", "Orchestrator Management Service", "Running","Auto");
-		verifyWinSerStatus("drscrhappprdn01.na.gilead.com", "Orchestrator Remoting Service", "Running", "Auto");
-		verifyWinSerStatus("drscrhappprdn01.na.gilead.com", "Orchestrator Runbook Service", "Running", "Auto");
+		verifyWinSerStatus("drscrhappprdn01", "Orchestrator Management Service", "Running","Auto");
+		verifyWinSerStatus("drscrhappprdn01", "Orchestrator Remoting Service", "Running", "Auto");
+		verifyWinSerStatus("drscrhappprdn01", "Orchestrator Runbook Service", "Running", "Auto");
 		
 	}
 	

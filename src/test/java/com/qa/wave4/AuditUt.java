5879bd4e14947081c5f4baffc5d55a40264c52bb
MarkantiDeepak <72135291+MarkantiDeepak@users.noreply.github.com>
2020-10-07 16:13:30 +0530

    Neww

--- src/test/java/com/qa/wave4/AuditUt.java
+++ src/test/java/com/qa/wave4/AuditUt.java
@@ -4,15 +4,22 @@ import org.testng.annotations.Test;
 
 
 import org.testng.annotations.AfterMethod;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -51,6 +58,31 @@ public static WebDriver driver;
 		WebElement AuditUtopia_ipt = createWebElementBy(ipt_AuditUtopia);
 		String AU = AuditUtopia_ipt.getText();
 		Assert.assertEquals(AU, "AuditUtopia");
+	}
+		public static void takeScreenshotAtEndOfTest() {
+			
+
+			ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
+				public Boolean apply(WebDriver driver) {
+					return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString()
+							.equals("complete");
+				}
+			};
+			try {
+				WebDriverWait wait = new WebDriverWait(driver, 30);
+				wait.until(expectation);
+			} catch (Throwable error) {
+				Assert.fail("Timeout waiting for Page Load Request to complete.");
+			}
+
+			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			File screenShotName = new File(userDirectory + "/screenshots/" + System.currentTimeMillis() + ".png");
+			try {
+				FileUtils.copyFile(scrFile, screenShotName);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 		public void launchBrowser(String browser, String url) {
 

ead5312faac47798208afee4974b375c3e080861
MarkantiDeepak <72135291+MarkantiDeepak@users.noreply.github.com>
2020-10-13 16:04:19 +0530

    Pilot changes

---
+++ src/test/java/com/qa/wave2/PRTGNetworkMonitor.java
@@ -0,0 +1,141 @@
+package com.qa.wave2;
+
+import org.testng.annotations.Test;
+
+import com.qa.demo.base.CommonMethods2;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class PRTGNetworkMonitor extends CommonMethods2 {
+
+	public PRTGNetworkMonitor() throws Exception {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/***************************** Test Cases *******************/
+
+	@BeforeMethod(enabled = true)
+	public void BeforeMethod() throws IOException {
+
+	}
+
+	@Test(priority = 0, enabled = true)
+	public void PRTGNM_S01_Web_Devurl_UserLogin() throws IOException {
+		GM_PRTGNM_Web_DevURLLogin();
+	}
+
+	@Test(priority = 1, enabled = true)
+	public void PRTGNM_S02_Web_DevurlViewAllSensorsData() throws IOException {
+		TM_PRTGNM_S02_Web_Devurl_ViewAllSensorsData();
+	}
+
+	@Test(priority = 2, enabled = false)
+	public void PRTGNM_S03_Web_Produrl_UserLogin() throws IOException {
+		GM_PRTGNM_Web_ProdURLLogin();
+	}
+
+	@Test(priority = 3, enabled = false)
+	public void PRTGNM_S04_Web_Produrl_ViewAllSensorsData() throws IOException {
+		TM_PRTGNM_S04_Web_Produrl_ViewAllSensorsData();
+	}
+
+	@AfterMethod
+	public void afterEveryTest() {
+		driver.quit();
+	}
+
+	/***************************** Locators *******************/
+
+	By ipt_Username = By.xpath("//input[@id='loginusername']");
+	By ipt_Password = By.xpath("//input[@id='loginpassword']");
+	By btn_Login = By.xpath("//button[@class='loginbutton button big']");
+
+	/***************************** Test Case Methods *******************/
+
+	public void TM_PRTGNM_S02_Web_Devurl_ViewAllSensorsData() throws IOException {
+		GM_PRTGNM_Web_DevURLLogin();
+		waitForPageLoaded();
+		Assert.assertEquals(driver.getTitle(), "Welcome | PRTG Network Monitor (SJPRTGAPPDEVG01)",
+				"PRTG Network Monitor Home page not displayed");
+		takeScreenshotAtEndOfTest();
+	}
+
+	public void TM_PRTGNM_S04_Web_Produrl_ViewAllSensorsData() throws IOException {
+
+		GM_PRTGNM_Web_ProdURLLogin();
+		waitForPageLoaded();
+		Assert.assertEquals(driver.getTitle(), "Welcome | PRTG Network Monitor (SJPRTGAPPPRDN01)",
+				"PRTG Network Monitor Home page not displayed");
+		takeScreenshotAtEndOfTest();
+
+	}
+
+	/***************************** Generic Methods *******************/
+
+	public void GM_PRTGNM_Web_DevURLLogin() throws IOException {
+
+		final String Browser = "Chrome";
+		final String DevURL = "http://sjprtgappdevg01/";
+		final String Username = "VMS";
+		String DecryptPassword = null;
+		final String EncryptPassword = "dLYEuUGwlY262xhKocrIUw==";
+		final String URL = DevURL; 
+		launchBrowser(Browser, URL);
+		try {
+			DecryptPassword = decrypt(EncryptPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		WebElement Username_Ipt = createWebElementBy(ipt_Username);
+		Username_Ipt.clear();
+		Username_Ipt.sendKeys(Username);
+
+		WebElement Password_Ipt = createWebElementBy(ipt_Password);
+		Password_Ipt.clear();
+		Password_Ipt.sendKeys(DecryptPassword);
+
+		WebElement Login_Btn = createWebElementBy(btn_Login);
+		Login_Btn.click();
+
+		waitForPageLoaded();
+		takeScreenshotAtEndOfTest();
+
+	}
+
+	public void GM_PRTGNM_Web_ProdURLLogin() throws IOException {
+
+		final String Browser = "Chrome";
+		final String ProdURL = "http://sjprtgappprdn01/";
+		final String Username = "dmarkanti";
+		String DecryptPassword = null;
+		final String EncryptPassword = "k7zK/nqaaa80bH7tAi7jZw==";
+		final String URL = ProdURL;
+		launchBrowser(Browser, URL);
+		try {
+			DecryptPassword = decrypt(EncryptPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		WebElement Username_Ipt = createWebElementBy(ipt_Username);
+		Username_Ipt.clear();
+		Username_Ipt.sendKeys(Username);
+
+		WebElement Password_Ipt = createWebElementBy(ipt_Password);
+		Password_Ipt.clear();
+		Password_Ipt.sendKeys(DecryptPassword);
+
+		WebElement Login_Btn = createWebElementBy(btn_Login);
+		Login_Btn.click();
+
+		waitForPageLoaded();
+		takeScreenshotAtEndOfTest();
+
+	}
+}
---
+++ src/test/java/com/qa/wave3/SCORCH_UI.java
@@ -0,0 +1,128 @@
+package com.qa.wave3;
+
+import org.testng.annotations.Test;
+
+import com.qa.demo.base.CommonMethods2;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Key;
+import org.sikuli.script.KeyModifier;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+import org.testng.Assert;
+import org.testng.Reporter;
+
+public class SCORCH_UI extends CommonMethods2 {
+	
+	public SCORCH_UI() throws Exception {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/***************************** Test Data *******************/
+
+	final String userDirectory = System.getProperty("user.dir");
+	String path = userDirectory + "\\AutoFiles\\SiKuli\\";
+
+	/***************************** Test Cases *******************/
+
+	@BeforeMethod(enabled = true)
+	public void BeforeMethod() throws IOException {
+
+	}
+
+	@Test(priority = 0, enabled = true)
+	public void SCORCH_Web_Dev_Webportal() throws IOException {
+		TM_SCORCH_S01_Web_Dev_Webportal();
+	}
+
+	@Test(priority = 1, enabled = false)
+	public void SCORCH_Web_Prod_Webportal() throws IOException {
+		TM_SCORCH_S02_Web_Prod_Webportal();
+	}
+
+	@AfterMethod(enabled = true)
+	public void afterEveryTest() {
+		driver.quit();
+	}
+
+	/***************************** Test Case Methods *******************/
+
+	public void TM_SCORCH_S01_Web_Dev_Webportal() throws IOException {
+		final String Browser = "IE";
+		final String DevURL = "http://sjscrhappdevn01:82/";
+		final String URL = DevURL;
+		launchBrowser(Browser, URL);
+		Screen S = new Screen();
+		Pattern RunbookServers = new Pattern(path + "SC_RBServers.PNG");
+		Pattern Instances= new Pattern(path + "SC_Instances.PNG");
+		Pattern Events = new Pattern(path + "SC_Events.PNG");
+		Pattern EventDetails= new Pattern(path + "SC_EventDetails.PNG");
+		
+
+		try {
+
+			S.doubleClick(RunbookServers);
+			wait(10);
+
+			S.exists(Instances);
+			Reporter.log("Runbook servers details are displayed");
+			takeScreenshotAtSikuli();
+			
+			S.doubleClick(Events);
+			wait(10);
+
+			S.exists(EventDetails);
+			Reporter.log("Events details are displayed");
+			takeScreenshotAtSikuli();
+			
+			
+
+
+		} catch (FindFailed e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void TM_SCORCH_S02_Web_Prod_Webportal() throws IOException {
+		
+		final String Browser = "IE";
+		final String ProdURL = "http://sjscrhappprdn01:82/ ";
+		final String URL = ProdURL;
+		launchBrowser(Browser, URL);
+		Screen S = new Screen();
+		Pattern RunbookServers = new Pattern(path + "SC_RBServers.PNG");
+		Pattern Instances= new Pattern(path + "SC_Instances.PNG");
+		Pattern Events = new Pattern(path + "SC_Events.PNG");
+		Pattern EventDetails= new Pattern(path + "SC_EventDetails.PNG");
+		
+
+		try {
+
+			S.doubleClick(RunbookServers);
+			wait(10);
+
+			S.exists(Instances);
+			Reporter.log("Runbook servers details are displayed");
+			takeScreenshotAtSikuli();
+			
+			S.doubleClick(Events);
+			wait(10);
+
+			S.exists(EventDetails);
+			Reporter.log("Events details are displayed");
+			takeScreenshotAtSikuli();
+			
+			} catch (FindFailed e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
